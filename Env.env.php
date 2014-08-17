92ca1837b7616ee3aad5facf793094369542c646
Campuslane <info@campuslane.com>
2014-08-17 10:24:47 -0700

    added starter env var files

---
+++ Env.env.php
@@ -0,0 +1,12 @@
+<?php 
+
+	// production environmental vars
+	putenv("DB_HOST = '127.0.0.1'");
+	putenv("DB_NAME" = 'starter');
+	putenv("DB_USER" = 'root');
+	putenv("DB_PASS" = 'password');
+	putenv("DB_PREFIX" = '');
+	putenv("APP_KEY" = '');
+	
+
+
