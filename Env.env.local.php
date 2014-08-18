ed8be443d715dbfc06f62591645f0d42586224bf
Campuslane <info@campuslane.com>
2014-08-18 14:29:38 -0700

    updated default theme

--- Env.env.local.php
+++ Env.env.local.php
@@ -5,7 +5,7 @@
 		'DB_HOST'	=> 	'127.0.0.1',
 		'DB_NAME'	=>	'database_name_here',
 		'DB_USER'	=>	'root',
-		'DB_PASS'	=>	'pass here',
+		'DB_PASS'	=>	'pass_here',
 		'DB_PREFIX'	=>	''
 	];
 	
--- Env.env.php
+++ Env.env.php
@@ -5,9 +5,9 @@
 		'DB_HOST'	=> 	'127.0.0.1',
 		'DB_NAME'	=>	'database_name_here',
 		'DB_USER'	=>	'root',
-		'DB_PASS'	=>	'pass here',
+		'DB_PASS'	=>	'pass_here',
 		'DB_PREFIX'	=>	'',
-		'APP_KEY'	=>	'app key here'
+		'APP_KEY'	=>	'app_key_here'
 	];
 	
 	
